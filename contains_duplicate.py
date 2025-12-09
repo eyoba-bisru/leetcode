2b5256dff2fada96542f2c84432f646b5318dc8f
Eyob <eabebe91@gmail.com>
2025-12-09 15:54:19 +0300

    Add solutions for containsDuplicate and missingNumber problems

---
+++ contains_duplicate.py
@@ -0,0 +1,14 @@
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        dictionary = dict()
+        for num in nums:
+            dictionary[num] = 0
+
+        for num in nums:
+            dictionary[num] += 1
+        
+        for _, value in dictionary.items():
+            if value > 1:
+                return True
+    
+        return False
