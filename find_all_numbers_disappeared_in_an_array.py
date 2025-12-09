472e70b34b9a61ad327a95f59a403059585bbf9b
Eyob <eabebe91@gmail.com>
2025-12-09 16:08:52 +0300

    Add solution for findDisappearedNumbers problem and optimize missingNumber function

---
+++ find_all_numbers_disappeared_in_an_array.py
@@ -0,0 +1,4 @@
+class Solution:
+    def findDisappearedNumbers(self, nums: List[int]) -> List[int]:            
+        set_nums = set(nums)
+        return [i for i in range(1, len(nums)+1) if i not in set_nums]
--- missing_number.py
+++ missing_number.py
@@ -1,7 +1,8 @@
 class Solution:
     def missingNumber(self, nums: List[int]) -> int:
+        set_nums = set(nums)
         for i in range(len(nums) + 1):
-            if i not in nums:
+            if i not in set_nums:
                 return i
 
         return None
