065c9f4d2befb4c30ec387365abf3a1041a48716
Eyob <eabebe91@gmail.com>
2025-12-10 11:56:07 +0300

    Refactor smallerNumbersThanCurrent function for improved efficiency

--- how_many_numbers_are_smaller_than_the_current_number.py
+++ how_many_numbers_are_smaller_than_the_current_number.py
@@ -3,14 +3,16 @@ from typing import List
 
 class Solution:
     def smallerNumbersThanCurrent(self, nums: List[int]) -> List[int]:
-        count_arr = []
-        for i in range(len(nums)):
-            count = 0
-            for j in range(len(nums)):
-                if nums[j] < nums[i]:
-                    count += 1
-                
-            count_arr.append(count)
+
+        sorted_arr = sorted(nums)
+        d = {}
+
+        for i,num in enumerate(sorted_arr):
+            if num not in d:
+                d[num] = i
         
-        return count_arr
+        output_arr = []
+        for num in nums:
+            output_arr.append(d[num])
         
+        return output_arr
