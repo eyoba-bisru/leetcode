4fea014a5aa61307f553cd9bab0934c8cddf74a6
Eyob <eabebe91@gmail.com>
2025-12-10 12:03:57 +0300

    Simplify containsDuplicate function by using set for duplicate detection

--- contains_duplicate.py
+++ contains_duplicate.py
@@ -3,15 +3,4 @@ from typing import List
 
 class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:
-        dictionary = dict()
-        for num in nums:
-            dictionary[num] = 0
-
-        for num in nums:
-            dictionary[num] += 1
-        
-        for _, value in dictionary.items():
-            if value > 1:
-                return True
-    
-        return False
+        return len(set(nums)) != len(nums)
